3a4079e346bbf1341549859bd4ca66e12dac45c4
NyanWhite <2282983971@qq.com>
2025-03-30 21:14:59 +0800

    加入多线程
    相同或不同账号的多线程执行 有点吃ram）

--- Controller.py
+++ Controller.py
@@ -1,44 +1,56 @@
 '''
 Author: LetMeFly
-Date: 2024-07-23 16:31:29
-LastEditors: LetMeFly
-LastEditTime: 2024-07-24 12:42:46
+Date: 2023-09-12 20:49:21
+Modifier:NyanWhite
+Description: 开源于https://github.com/LetMeFly666/YuketangAutoPlayer 欢迎issue、PR
+后续改进版 https://github.com/NyanWhite/YuketangAutoPlay_Enhanced
+感谢支持w
 '''
 import os
+import threading
+import time
+from yuketang import YuketangPlayer, load_config
 
-watchList = [
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44371/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44333/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44370/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44372/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44362/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44366/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44360/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44353/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44359/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44321/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44350/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44320/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44355/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44354/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44349/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44345/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44347/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44339/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44313/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44344/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44329/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44328/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44327/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44310/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44318/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44330/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44323/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44315/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44307/studycontent',
-    'https://acgecfd.yuketang.cn/pro/lms/undefined/44370/studyconten',
+def main():
+    # 加载配置
+    config = load_config()
+    settings = config.get('settings', {})
+    courses = config.get('courses', [])
+    
+    max_concurrent = settings.get('max_concurrent', 3)
+    headless = settings.get('headless', True)
+    
+    print(f"配置加载完成，最大并发数: {max_concurrent}, 无头模式: {headless}")
+    print(f"共找到 {len(courses)} 个课程")
+    
+    # 创建并启动线程
+    active_threads = []
+    
+    for course in courses:
+        # 等待有空闲线程槽位
+        while len(active_threads) >= max_concurrent:
+            # 移除已完成的线程
+            active_threads = [t for t in active_threads if t.is_alive()]
+            if len(active_threads) >= max_concurrent:
+                print(f"当前活跃线程数: {len(active_threads)}，等待空闲槽位...")
+                time.sleep(5)
+        
+        # 启动新线程
+        print(f"正在启动课程: {course['url']}")
+        player = YuketangPlayer(course['url'], course['cookie'], headless=headless)
+        player.start()
+        active_threads.append(player)
+        time.sleep(1)  # 避免同时启动造成资源竞争
+    
+    # 等待所有线程完成
+    print("所有课程已启动，等待完成...")
+    while active_threads:
+        active_threads = [t for t in active_threads if t.is_alive()]
+        if active_threads:
+            print(f"剩余活跃课程: {len(active_threads)}")
+            time.sleep(10)
+    
+    print("所有课程已完成！")
 
-]
-
-for thisUrl in watchList:
-    os.system(f'python yuketang.py "{thisUrl}"')
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -6,15 +6,23 @@ Based https://github.com/LetMeFly666/YuketangAutoPlayer/ 并进行改进
 
 ##  改进了什么东西呢？
 
-- [x] 外置刷课地址与登录使用的Cooke
+- [x] 外置无头模式设定 刷课地址与登录使用的Cookie
 - [x] 去除ChromeWebDriver的PATH项 现在仅需放在相同目录即可
-- [ ] 并行刷课（多课程同时
+- [x] 多线程刷课（支持自定线程数）
+  *但不能用在同一个课程上 这会导致所有行为都只指向同一个视频*
+- [x] 多账号刷课进行
+  不是哥们可以这样？ 关于这条请看下方的config.json配置
 - [x] 获取ChromeWebDriver并下载
   不完全实现 还是建议走原repo的教程
 
 - [x] ~~更优质的睡眠~~
 
 不会Code 基于LLM帮助修改而来 让我们感谢.jpg
+
+---
+
+由于被改的莫名其妙的所以现在只要运行Controller.py即可 但yuketang.py还能不能用得上暂且未知（）
+先放着
 
 ---
 
@@ -30,15 +38,41 @@ pip install selenium requests urllib3 tqdm
 
 ### 配置方法
 
-仅需修改其中的config.json 填入指向的课程地址和Cookie即可
-Cookie也可不填 在尝试访问时会跳出二维码供扫描 但是忘记扫了的话会被退出（未来也许会修正
-
-不会写的话可参考原repo 此处相当于将敏感信息分离
+Cookie请看原repo进行配置 其他的根据这个repo即可
 
 ```json
 {
-    "COURSE_URL": "此处是需要刷的地址",
-    "COOKIE": "填入Cookie 可为空"
-}
+    "settings": {
+      "max_concurrent": 此处是最大线程数(也就是同时进行的课程种类数量_纯数字即可),
+      "headless": 无头模式的启用与否_正常运行的话就可以开启了(false/true)
+    },
+    "courses": [
+      {
+        "url": "课程种类1的链接",
+        "cookie": "Cookie1"
+      },
+      {
+        "url": "课程种类2的链接",
+        "cookie": "Cookie2"
+      },
+      {
+        "url": "课程种类3的链接",
+        "cookie": "Cookie3"
+      },
+      {
+        "url": "课程种类4的链接",
+        "cookie": "Cookie4"
+      },
+      {
+       如上 并进行略微的修改（如果需要大于4的话）
+      }
+    ]
+  }
 ```
 
+Notice:这里的Cookie1 2 3 4都可以是相同的 也就是可以以相同或不同的账号进行多个课程的自动进行
+
+---
+
+本人不对这个奇异玩意作任何解释说明 反正能跑就行（）
+its works fine on my pc()
